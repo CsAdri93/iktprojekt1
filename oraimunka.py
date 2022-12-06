65c76cb57726757d062f1c55238b32cb8cb0b029
Csizmadia Adrienn <CsizmadiaA@kkszki.local>
2022-12-06 16:21:39 +0100

    feladatok

---
+++ oraimunka.py
@@ -0,0 +1,19 @@
+#1, legkisebb érték
+
+#2, legnagyobb érték
+
+#3, érdemjegy
+
+#4, osztható-e
+
+#5, kettő összege
+
+#6, páros szám
+
+#7, abc sorrend
+
+#8, kisebb
+
+#9, zárt intervallum
+
+#10, start
